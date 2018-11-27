37ef620a63cf43df870f04a93fb44cf47dcf81cb
Microclimate User <microclimate.user@localhost>
2018-11-27 17:22:07 +0000

    commit 1

--- src/main/java/application/SBApplication.java
+++ src/main/java/application/SBApplication.java
@@ -3,12 +3,34 @@ package application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 @SpringBootApplication
 @ComponentScan(basePackages = {"application", "com.ibm.javametrics.spring"})
 public class SBApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(SBApplication.class, args);
+
+        URL url = new URL("http://testejava.default.svc.cluster.local:8080/health");
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		String inputLine;
+		StringBuffer response = new StringBuffer();
+
+		while ((inputLine = in.readLine()) != null) {
+			response.append(inputLine);
+		}
+		in.close();
+
+		//print result
+        System.out.println("------------------");
+		System.out.println(response.toString());
+        System.out.println("******************");
     }
 }
